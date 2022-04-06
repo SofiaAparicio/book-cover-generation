c7be3ed3f900310d820189a63459714019329f40
SSamDav <ssam_dav@hotmail.com>
2022-04-06 01:15:35 +0100

    Add threading parallelism

--- src/scrapper/goodreads.py
+++ src/scrapper/goodreads.py
@@ -1,10 +1,12 @@
+import sys
 import re
+from multiprocessing import Pool
 from typing import Any, Dict, List, Union
-from bs4 import BeautifulSoup
 
 import requests
-import time
-import random
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
 ROOT_URL = 'https://www.goodreads.com/'
 
@@ -21,15 +23,34 @@ def get_book_info(url: str) -> Dict[str, Any]:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    title, author = soup.find('title').string.split('by')
-    image_url = soup.find('meta', property='og:image')['content']
-    isbn13 = soup.find('meta', property='books:isbn')['content']
-    num_pages = soup.find('meta', property='books:page_count')['content']
-    avg_rating = soup.find('span', {'itemprop': 'ratingValue'}).string.split('\n')[1].strip()
-    num_ratings = soup.find('meta', {'itemprop': 'ratingCount'}).string.split('\n')[1].strip()
-    num_reviews = soup.find('meta', {'itemprop': 'reviewCount'}).string.split('\n')[1].strip()
+    title, author = None, None
+    title_author = soup.find('title')
+    title_author = title_author.string.split(' by ')
+    title = title_author[0].strip()
+    if len(title_author) > 1: author = title_author[1].strip()
+
+    image_url = soup.find('meta', property='og:image')
+    if image_url is not None: image_url = image_url['content']
+
+    isbn13 = soup.find('meta', property='books:isbn')
+    if isbn13 is not None: isbn13 = isbn13['content']
+
+    num_pages = soup.find('meta', property='books:page_count')
+    if num_pages is not None: num_pages = num_pages['content']
+
+    avg_rating = soup.find('span', {'itemprop': 'ratingValue'})
+    if avg_rating is not None: avg_rating = avg_rating.string.split('\n')[1].strip()
+
+    num_ratings = soup.find('meta', {'itemprop': 'ratingCount'})
+    if num_ratings is not None: num_ratings = num_ratings.string.split('\n')[1].strip()
+
+    num_reviews = soup.find('meta', {'itemprop': 'reviewCount'})
+    if num_reviews is not None: num_reviews = num_reviews.string.split('\n')[1].strip()
+
+    
     language = soup.find('div', {'itemprop': 'inLanguage'})
     if language is not None: language = language.string
+    
     genres = [genre.string for genre in soup.find('body').find_all('a', {'class': 'actionLinkLite bookPageGenreLink'})]
 
     return {
@@ -77,7 +98,15 @@ def get_genres() -> List[Dict[str, Union[str, int]]]:
     return genre_list
 
 
-def get_books(genre: str, min_sleep: float=0.5, max_sleep: float=1.5) -> List[Dict[str, Any]]:
+def get_books(genre: str) -> List[str]:
+    """Get books links for a given genre.
+
+    Args:
+        genre (str): Book genre.
+
+    Returns:
+        List[str]: Book URLs.
+    """    
     URL = "shelf/show/{genre}?page={page}"
     page = 1 
     books = []
@@ -89,18 +118,16 @@ def get_books(genre: str, min_sleep: float=0.5, max_sleep: float=1.5) -> List[Di
     number_books = data.select_one('div[class=leftContainer] > div[class=mediumText] > span[class=smallText]').string
     max_number_books = int(re.search(r'Showing \d{1,3}-\d{1,3} of (\d+(,\d+)?)', number_books).group(1).replace(',',''))
 
+    pbar = tqdm(total=max_number_books, desc=f"Scrapping {genre} books links", leave=False)
+
     while max_number_books > 0:
         all_books = data.select('div[class=leftContainer] > div[class=elementList]')
         for book in all_books:
             url_extension = book.find("a", {"class":"bookTitle"})['href']
-            book_url = ROOT_URL + url_extension
-            book_info = get_book_info(book_url)
-            books.append(book_info)
-
+            book_url = ROOT_URL + re.sub(r'^/', '', url_extension)
+            books.append(book_url)
             max_number_books -= 1
-        
-        sleep = random.uniform(min_sleep, max_sleep)
-        time.sleep(sleep)
+            pbar.update(1)
         
         page += 1
         url = ROOT_URL + URL.format(genre=genre, page=page)
@@ -108,3 +135,21 @@ def get_books(genre: str, min_sleep: float=0.5, max_sleep: float=1.5) -> List[Di
         data = BeautifulSoup(html, 'html.parser')
 
     return books
+
+
+def scrapp_books_multiprocess(genre: str, num_proc: int = 10) -> List[Dict[str, Any]]:
+    """Scraps all the books from one genre using multiprocessing.
+
+    Args:
+        genre (str): Book genre to be scrapped.
+        num_proc (int, optional): Numeber of processores to be used. Defaults to 10.
+
+    Returns:
+        List[Dict[str, Any]]: Book information.
+    """    
+    book_links = get_books(genre)
+    
+    with ThreadPoolExecutor(max_workers=num_proc) as executor:
+        books_info = list(tqdm(executor.map(get_book_info, book_links), total=len(book_links), desc= f'Scrapping {genre} books', leave=False))
+
+    return books_info
